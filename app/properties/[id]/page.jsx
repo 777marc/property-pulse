6304589f07ae6f1e718e02ea2c6efbda3bed4557
777marc@gmail.com <777marc@gmail.com>
2024-03-29 11:30:59 -0400

    signle property route

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,7 +1,31 @@
 "use client";
-import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import { fetchProperty } from "@/utils/requests";
 
 const SingleProperty = () => {
+  const { id } = useParams();
+  const [property, setProperty] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPropertyData = async () => {
+      if (!id) return null;
+
+      try {
+        const property = await fetchProperty(id);
+        if (property) setProperty(property);
+      } catch (error) {
+        console.log(`Error fetching property: ${error}`);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    if (property === null) {
+      fetchPropertyData();
+    }
+  }, [id, property]);
+
   return <div>SingleProperty</div>;
 };
 
--- utils/requests.js
+++ utils/requests.js
@@ -1,5 +1,26 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
+// fetch all properties
+async function fetchProperty(id) {
+  try {
+    if (!apiDomain) {
+      return null;
+    }
+
+    const res = await fetch(`${apiDomain}/properties/${id}`);
+
+    if (!res.ok) {
+      throw new Error("failed to fetch data");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
+
+// fetch single property
 async function fetchProperties() {
   try {
     if (!apiDomain) {
@@ -19,4 +40,4 @@ async function fetchProperties() {
   }
 }
 
-export { fetchProperties };
+export { fetchProperties, fetchProperty };
