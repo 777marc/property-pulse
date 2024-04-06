84c4766d8f7276c8d31c750e6ab906a788a6287b
777marc@gmail.com <777marc@gmail.com>
2024-04-06 14:03:39 -0400

    add logged in user to db

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,4 +1,6 @@
 import GoogleProvider from "next-auth/providers/google";
+import connectDB from "@/config/database";
+import User from "@/models/User";
 
 export const authOptions = {
   providers: [
@@ -17,16 +19,33 @@ export const authOptions = {
   callbacks: {
     // invokes on successful sign in
     async signIn({ profile }) {
+      const { email, name, picture: image } = profile;
+
       // 1. Connect to DB
+      await connectDB();
       // 2. Check if user exists
+      const userExists = await User.findOne({ email });
       // 3. If not, then add user to DB
+      if (!userExists) {
+        const username = name.slice(0, 20);
+
+        await User.create({
+          email,
+          username,
+          image,
+        });
+      }
       // 4. Return true to allow sign in
+      return true;
     },
     // Modify session object
     async session({ session }) {
       // 1. Get user from DB
+      const user = await User.findOne({ email: session.user.email });
       // 2. Assign the user to the session
+      session.user.id = user._id.toString();
       // 3. Return session
+      return session;
     },
   },
 };
