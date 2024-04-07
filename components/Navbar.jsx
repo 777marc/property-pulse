792e903925d0ce4a9e12ba5ebeb6c0cc05d23f81
Marc Mendoza <777marc@gmail.com>
2024-04-07 09:25:40 -0400

    mobile login button

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -246,11 +246,20 @@ const Navbar = () => {
                 Add Property
               </Link>
             )}
-            {!session && (
-              <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4">
-                <span>Login or Register</span>
-              </button>
-            )}
+            {!session &&
+              providers &&
+              Object.values(providers).map((provider, index) => {
+                return (
+                  <button
+                    key={index}
+                    className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                    onClick={() => signIn(provider.id)}
+                  >
+                    <FaGoogle className="text-white mr-2" />
+                    <span>Login or Register</span>
+                  </button>
+                );
+              })}
           </div>
         </div>
       )}
