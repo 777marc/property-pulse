5be95fde24b552ccd15d644639d4d92f7f26083c
Marc Mendoza <777marc@gmail.com>
2024-04-17 17:20:02 -0400

    upload images to cloudinary

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,6 +1,8 @@
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
 import { getSessionUser } from "@/utils/getSessionUser";
+import cloudinary from "@/config/cloudinary";
+
 // GET /api/properties
 export const GET = async (request) => {
   try {
@@ -59,10 +61,34 @@ export const POST = async (request) => {
         phone: formData.get("seller_info.phone"),
       },
       owner: sessionUser.userId,
-      // images,
     };
 
-    console.log(propertyData);
+    // upload images to cloudinary
+    const imageUploadPromises = [];
+
+    for (const image of images) {
+      const imageBuffer = await image.arrayBuffer();
+      const imageArray = Array.from(new Uint8Array(imageBuffer));
+      const imageData = Buffer.from(imageArray);
+
+      // convert to base64
+      const imageBase64 = imageData.toString("base64");
+
+      // make upload request
+      const result = await cloudinary.uploader.upload(
+        `data:image/png;base64,${imageBase64}`,
+        {
+          folder: "property-pulse",
+        }
+      );
+
+      imageUploadPromises.push(result.secure_url);
+
+      // wait for all images to upload
+      const uploadedImages = await Promise.all(imageUploadPromises);
+      // add images to propertyData object
+      propertyData.images = uploadedImages;
+    }
 
     const newProperty = new Property(propertyData);
     await newProperty.save();
