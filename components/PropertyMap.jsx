b1b2bb36d6127c138bf0863b4eb23c2e1d344c05
Marc Mendoza <777marc@gmail.com>
2024-05-03 17:39:39 -0400

    misc map work

---
+++ components/PropertyMap.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+import "mapbox-gl/dist/mapbox-gl.css";
+import Map, { Marker } from "react-map-gl";
+import { setDefaults, fromAddress } from "react-geocode";
+import Spinner from "./Spinner";
+import Image from "next/image";
+import pin from "@/assets/images/pin.svg";
+
+const PropertyMap = () => {
+  return <div>PropertyMap</div>;
+};
+
+export default PropertyMap;
