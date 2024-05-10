0b6605f62e0129436534285608bdcfe4d60fd770
Marc Mendoza <777marc@gmail.com>
2024-05-10 14:57:29 -0400

    save properties page

--- app/api/bookmarks/route.js
+++ app/api/bookmarks/route.js
@@ -1,8 +1,33 @@
 import connectDB from "@/config/database";
 import User from "@/models/User";
+import Property from "@/models/Property";
 import { getSessionUser } from "@/utils/getSessionUser";
 
 export const dynamic = "force-dynamic";
+
+// GET /api/bookmarks
+export const GET = async (request) => {
+  console.log("here.....");
+  try {
+    await connectDB();
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+    // 1. get bookmarks
+    const { userId } = sessionUser;
+    const user = await User.findById(userId);
+
+    // 2. get properties from bookmarks
+    const properties = await Property.find({ _id: { $in: user.bookmarks } });
+
+    return new Response(JSON.stringify({ properties }), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response("something went wrong", { status: 500 });
+  }
+};
 
 // POST /api/bookmarks
 export const POST = async (request) => {
---
+++ app/properties/saved/page.jsx
@@ -0,0 +1,54 @@
+"use client";
+import { useEffect, useState } from "react";
+import PropertiesCard from "@/components/PropertiesCard";
+import Spinner from "@/components/Spinner";
+import { toast } from "react-toastify";
+
+const SavedPropertiesPage = () => {
+  const [properties, setProperties] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getProperties = async () => {
+      try {
+        const res = await fetch(`/api/bookmarks`);
+        if (res.status === 200) {
+          const data = await res.json();
+          setProperties(data.properties);
+        }
+      } catch (error) {
+        console.log(error);
+        toast.error("something went wrong.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getProperties();
+  }, []);
+
+  if (loading) return <Spinner loading={loading} />;
+
+  return (
+    <>
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto px-4 py-6">
+          {properties.lenth === 0 ? (
+            <p>No Properties Found</p>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {properties.map((property) => {
+                return (
+                  <PropertiesCard key={property._id} property={property} />
+                );
+              })}
+            </div>
+          )}
+        </div>
+      </section>
+      ;
+    </>
+  );
+};
+
+export default SavedPropertiesPage;
