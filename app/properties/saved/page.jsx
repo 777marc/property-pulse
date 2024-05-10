71caf55fe86af322daf17b3d77b0a147463ccc37
Marc Mendoza <777marc@gmail.com>
2024-05-10 15:08:58 -0400

    saved properties clean up

--- app/properties/saved/page.jsx
+++ app/properties/saved/page.jsx
@@ -33,6 +33,7 @@ const SavedPropertiesPage = () => {
     <>
       <section className="px-4 py-6">
         <div className="container-xl lg:container m-auto px-4 py-6">
+          <h1 className="text-2xl mb-4">Saved Properties</h1>
           {properties.lenth === 0 ? (
             <p>No Properties Found</p>
           ) : (
--- components/PropertiesCard.jsx
+++ components/PropertiesCard.jsx
@@ -85,7 +85,7 @@ const PropertiesCard = ({ key, property }) => {
           </div>
           <Link
             href={`/properties/${property._id}`}
-            className="h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+            className="h-[40px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm"
           >
             Details
           </Link>
