1dd4f7d8a8481cd458425c29adc148ce11dccc20
Marc Mendoza <777marc@gmail.com>
2024-05-16 17:32:17 -0400

    search results page

--- app/properties/search-results/page.jsx
+++ app/properties/search-results/page.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+"use client";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import Spinner from "@/components/Spinner";
+import PropertiesCard from "@/components/PropertiesCard";
 
-const page = () => {
-  return <div>Search Results</div>;
+const SearchResultsPage = () => {
+  const [properties, setProperties] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const searchParams = useSearchParams();
+  const location = searchParams.get("location");
+  const propertyType = searchParams.get("propertyType");
+
+  useEffect(() => {
+    const getSearchResults = async () => {
+      try {
+        const res = await fetch(
+          `/api/properties/search?location=${location}&propertyType=${propertyType}`
+        );
+
+        if (res.status === 200) {
+          const data = await res.json();
+          setProperties(data);
+        }
+      } catch (error) {
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getSearchResults();
+  }, []);
+
+  if (loading) return <Spinner loading={loading} />;
+
+  return (
+    <>
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto px-4 py-6">
+          {properties.lenth === 0 ? (
+            <p>No Properties Found</p>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {properties.map((property) => {
+                return (
+                  <PropertiesCard key={property._id} property={property} />
+                );
+              })}
+            </div>
+          )}
+        </div>
+      </section>
+    </>
+  );
 };
 
-export default page;
+export default SearchResultsPage;
