43b729c00fb8e450fcaaa200693d8c2e61464134
Marc Mendoza <777marc@gmail.com>
2024-05-29 10:03:40 -0400

    message card work

--- components/Messages.jsx
+++ components/Messages.jsx
@@ -29,7 +29,26 @@ const Messages = () => {
 
   if (loading) return <Spinner loading={loading} />;
 
-  return <MessageCard message={{ sender: "123" }} />;
+  return (
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {messages.lenth === 0 ? (
+          <p>No messages Found</p>
+        ) : (
+          <section class="bg-blue-50">
+            <div class="container m-auto py-24 max-w-6xl">
+              <div class="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+                <h1 class="text-3xl font-bold mb-4">Your Messages</h1>
+                {messages.map((message) => {
+                  return <MessageCard key={message._id} message={message} />;
+                })}
+              </div>
+            </div>
+          </section>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default Messages;
